c8287b13023dea55fb282f6605e82f41a266f374
Mahesh Bisl <mbisl@infomedia.com.au>
2019-04-03 19:36:36 +1100

    added VPC cloudformation

--- build-deploy.sh
+++ build-deploy.sh
@@ -4,10 +4,14 @@ mvn integration-test
 ls target/mahesh-webserver.war
 ln -f target/mahesh-webserver.war packer/mahesh-webserver.war
 
+cd sceptre
+sceptre launch --yes vpc-alb-rds
+cd -
+
 # Packer build
 cd packer
 packer build packer.json | tee /tmp/ami-output
-AMI=$(tail -5 /tmp/ami-output | grep -o 'ami-.*')
+export AMI=$(tail -5 /tmp/ami-output | grep -o 'ami-.*')
 cd -
 
 echo ${AMI}
