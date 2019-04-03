2984c355e038cec020b8626d38dc482c21aff55f
Mahesh Bisl <mbisl@infomedia.com.au>
2019-04-03 19:10:09 +1100

    check if jq, sceptre and aws cli are installed properly

--- install.sh
+++ install.sh
@@ -6,4 +6,7 @@ sudo curl -OL https://releases.hashicorp.com/packer/1.3.5/packer_1.3.5_linux_amd
 sudo apt-get install -y jq python3 python3-pip
 
 sudo pip3 install --upgrade pip
-sudo pip3 install --upgrade -r requirements.txt
+sudo pip3 install --upgrade -r requirements.txt && \
+    sceptre --version && \
+    aws --version && \
+    jq --version
